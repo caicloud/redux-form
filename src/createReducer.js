9ee9d940ed541dd4b8089ef07e7aa38a737f3345
Yao Zhang <zhangyao823785@gmail.com>
2019-06-20 18:35:22 +0800

    fix(onBlur): making onBlur not update the form value (#13)
    * fix(onBlur): making onBlur not update the form value
    
    * bump to 1.0.2-alpha1

--- src/createReducer.js
+++ src/createReducer.js
@@ -198,9 +198,11 @@ function createReducer<M, L>(structure: Structure<M, L>) {
       const initial = getIn(result, `initial.${field}`)
       if (initial === undefined && payload === '') {
         result = deleteInWithCleanUp(result, `values.${field}`)
-      } else if (payload !== undefined) {
-        result = setIn(result, `values.${field}`, payload)
       }
+      // Prevent blur reducer sets value in store.
+      // else if (payload !== undefined) {
+      //   result = setIn(result, `values.${field}`, payload)
+      // }
       if (field === getIn(result, 'active')) {
         result = deleteIn(result, 'active')
       }
